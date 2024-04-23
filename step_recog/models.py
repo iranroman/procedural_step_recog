0f78d626b275413d4065c0280b97e9c0364fc997
Bea Steers <bea.steers@gmail.com>
2024-04-23 10:25:54 -0400

    minimal changes

--- step_recog/models.py
+++ step_recog/models.py
@@ -43,17 +43,30 @@ class StepNet(nn.Module):
         self.gru_states = nn.GRU(cfg['DATASET']['NSTEPS'],cfg['MODEL']['GRU_INPUT_SIZE'],1)
         self.gru_dense_state_machine = nn.Linear(cfg['MODEL']['GRU_INPUT_SIZE'],(cfg['DATASET']['NSTEPS']-1)*cfg['DATASET']['NMACHINESTATES'])
 
+        self.use_state_head = cfg['MODEL']['USE_STATE_HEAD']
         self.relu = nn.ReLU()
         #self.softmax = nn.Softmax(-1)
+        self.SKILL_ID = cfg['MODEL']['SKILL_ID']
+        self.SKILL_STEPS = cfg['MODEL']['SKILL_STEPS']
 
+    def insert_image_buffer(self, x, output=None):
+        # (B, T, C, H, W)
+        x = torch.as_tensor(x).to(self.vid_mean.device)
+        if output is None:
+            return x.repeat(1, self.vid_nframes, 1, 1, 1)
+        if x.shape[1] >= output.shape[1]:
+            return x
+        return torch.cat([output[:, x.shape[1]:], x], dim=1)
 
-    def forward(self, x, skill=None):
+    def forward(self, x, h=None):
+        h_step, h_state = h if h is not None else (None, None)
+
+        # (B, T, C, H, W) -> (B, C, T, H, W)
+        x = x.permute(0,2,1,3,4)
+        x = (x - self.vid_mean) / (self.vid_std)
 
         vid_embeds = []
         omni_outs = []
-        x = x.permute(0,2,1,3,4)
-        x -= self.vid_mean
-        x /= self.vid_std
         for t in range(x.shape[2]-self.vid_nframes+1):
             x_vid = x[:,:,t:t+self.vid_nframes]
             assert x_vid.shape[2] == self.vid_nframes
@@ -63,15 +76,30 @@ class StepNet(nn.Module):
         vid_embeds = torch.stack(vid_embeds)
         omni_outs = torch.stack(omni_outs)
         vid_embeds = self.dropout(self.relu(self.video_dense(vid_embeds)))
-        gru_out,hidden = self.gru(vid_embeds)
+
+        # predict steps
+        gru_out, h_step = self.gru(vid_embeds)
         y_hat_steps = self.gru_dense_steps(gru_out).permute(1,0,2)
-        gru_out,hidden = self.gru_states(y_hat_steps)
-        y_hat_state_machine = self.gru_dense_state_machine(gru_out).permute(1,0,2)
-        y_hat_state_machine = y_hat_state_machine.reshape(y_hat_steps.shape[0],y_hat_steps.shape[1],y_hat_steps.shape[2]-1,-1)
+
+        # predict states
+        y_hat_state_machine=None
+        if self.use_state_head:
+            gru_out, h_state = self.gru_states(y_hat_steps)
+            y_hat_state_machine = self.gru_dense_state_machine(gru_out).permute(1,0,2)
+            y_hat_state_machine = y_hat_state_machine.reshape(y_hat_steps.shape[0],y_hat_steps.shape[1],y_hat_steps.shape[2]-1,-1)
+        
+        h = h_step, h_state
+        return y_hat_steps, y_hat_state_machine, omni_outs, h
+
+    def skill_step_proba(self, y_hat_steps, y_hat_state_machine, skill=None):
         if skill:
-            return y_hat_steps, y_hat_state_machine, omni_outs, __SKILL_STEPS__[skill].to(self.device)
-        else:
-            return y_hat_steps, y_hat_state_machine, omni_outs
+            index = self.SKILL_STEPS[skill]
+            y_hat_steps = y_hat_steps[:, :, index]
+            y_hat_state_machine = y_hat_state_machine[:, :, index[:-1]] if y_hat_state_machine is not None else None
+
+        y_hat_steps = torch.softmax(y_hat_steps, dim=-1)
+        y_hat_state_machine = torch.softmax(y_hat_state_machine, dim=-1) if y_hat_state_machine is not None else None
+        return y_hat_steps, y_hat_state_machine
 
 
 if __name__ == "__main__":
